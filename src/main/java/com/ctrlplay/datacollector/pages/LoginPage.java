330e262705704104789450afbca2544cbcd47fdb
Gabriel <gabrieldiogenes4@gmail.com>
2026-03-19 22:43:52 -0300

    feat: Adiciona dependencia Dotenv e adiciona string de usuario e senha no login

--- src/main/java/com/ctrlplay/datacollector/pages/LoginPage.java
+++ src/main/java/com/ctrlplay/datacollector/pages/LoginPage.java
@@ -4,6 +4,7 @@ import com.ctrlplay.datacollector.util.SeleniumUtils;
 import com.ctrlplay.datacollector.config.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class LoginPage {
     private WebDriver driver;
@@ -16,7 +17,17 @@ public class LoginPage {
         driver.navigate().to("https://loja.ctrlplay.com.br/login");
     }
     public void colocarUsuário(){
+        Dotenv dotenv = Dotenv.load();
         if(SeleniumUtils.foiEncontradoElementos(driver, By.cssSelector(""))){
+            String usuario = dotenv.get("LOGIN");
+
+        }
+    }
+
+    public void colocarSenha(){
+        Dotenv dotenv = Dotenv.load();
+        if(SeleniumUtils.foiEncontradoElementos(driver,By.cssSelector(""))){
+            String senha = dotenv.get("SENHA");
 
         }
     }
